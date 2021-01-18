f78869c395d407c4d9dea20c44ff41b69e7891ab
rodgersj097 <32108292+rodgersj097@users.noreply.github.com>
2021-01-18 09:43:37 -0500

    switched to sockets

---
+++ socket/socketFile.js
@@ -0,0 +1,28 @@
+const io = require("socket.io")();
+const torrent = require('../config/torrent')
+const socketapi = {
+    io: io
+};
+
+
+
+
+// Add your socket.io logic here!
+io.on("connection", function(socket) {
+    console.log("A user connected");
+    socket.on('getStats', (callback) => {
+        console.log('getting stats')
+        callback({
+            obj: torrent.getTorrentProgress()
+        })
+    })
+
+    socket.on('error', (callback) => {
+        callback({
+            error_message: torrent.getErrors()
+        })
+    })
+});
+// end of socket.io logic
+
+module.exports = socketapi;
